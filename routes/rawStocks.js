41521289e9e57f734ae198e1daa287403ad8cf93
GiorgioBaz <giorgio.bazouni@gmail.com>
2023-01-31 10:59:50 +1100

    Made a function to handle getting the tickers of gainers and losers.

--- routes/rawStocks.js
+++ routes/rawStocks.js
@@ -4,25 +4,45 @@ const Stocks = require("../models/stocks");
 require("dotenv").config({ path: "../config.env" });
 const yahooFinance = require("yahoo-finance2").default;
 
-app.get("/allStocksPM", async function (req, res) {
+//
+
+async function getCleanedStockData(all = false, gaining = false) {
     const stockTickers = await require("../StockTickers/topStocks");
-    // Stocks.find({}, (err, stock) => {
-    //     if (err) throw err;
-    //     if (!stock) res.send("Ya dun goofed");
-    //     else {
-    //         res.send(stock);
-    //     }
-    // });
-
     const stockData = await yahooFinance.quote(stockTickers);
-
-    const cleanedStockData = stockData.map((stock) => {
-        return {
-            symbol: stock.symbol,
-            changePercent: stock.regularMarketChangePercent,
-        };
+    let cleanedStockData = stockData.map((stock) => {
+        return stock.symbol;
     });
-    res.send(cleanedStockData);
+
+    if (all) return cleanedStockData;
+
+    if (gaining) {
+        return cleanedStockData.filter((stock) => stock.changePercent > 0);
+    } else if (!gaining) {
+        return cleanedStockData.filter((stock) => stock.changePercent < 0);
+    }
+}
+
+app.get("/allStocksPM", async function (req, res) {
+    const cleanedData = await getCleanedStockData(true);
+    res.send(cleanedData);
+});
+
+app.get("/gainers", async function (req, res) {
+    const cleanedData = await getCleanedStockData(false, true);
+    res.send(cleanedData);
+});
+
+app.get("/losers", async function (req, res) {
+    const cleanedData = await getCleanedStockData();
+    res.send(cleanedData);
+});
+
+app.get("/consecutiveGainers", async function (req, res) {
+    //check historical data for the last 8 days and store that in an array in mongodb :)
+    yahooFinance.historical("AAPL", {
+        period1: "2023-01-22",
+        period2: "2023-01-31",
+    });
 });
 
 module.exports = app;
