10cc64ba6129425f2c2a39b7a8f21c30071d0331
GiorgioBaz <giorgio.bazouni@gmail.com>
2023-02-02 19:51:28 +1100

    Fixed a bug in the calculation for isConsecutiveGainer. Created consecutiveLoser route and got all functionality working

--- routes/rawStocks.js
+++ routes/rawStocks.js
@@ -3,6 +3,9 @@ const app = express();
 const Stocks = require("../models/stocks");
 require("dotenv").config({ path: "../config.env" });
 const yahooFinance = require("yahoo-finance2").default;
+const moment = require("moment-business-days");
+
+const today = new Date();
 
 //
 
@@ -29,6 +32,17 @@ async function getCleanedStockData(all = false, gaining = false) {
     }
 }
 
+async function getHistoricalData(ticker, fromDate, toDate) {
+    return await yahooFinance.historical(ticker, {
+        period1: fromDate,
+        period2: toDate,
+    });
+}
+
+function getDateXDaysAgo(daysAgo) {
+    return moment(today).businessSubtract(daysAgo)._d;
+}
+
 app.get("/allStocksPM", async function (req, res) {
     const cleanedData = await getCleanedStockData(true);
     res.send(cleanedData);
@@ -48,55 +62,78 @@ app.get("/consecutiveGainers", async function (req, res) {
     //check historical data for the last 8 days and store that in an array in mongodb :)
     const gainers = await getCleanedStockData(false, true);
 
-    async function getHistoricalData(ticker, fromDate, toDate) {
-        return await yahooFinance.historical(ticker, {
-            period1: fromDate,
-            period2: toDate,
-        });
-    }
-
     function isConsecutiveGainer(quotes) {
         const consecutiveGainers = [];
         for (let i = 0; i < quotes.length; i++) {
-            if (i + 1 === quotes.length) break;
+            if (i + 1 === quotes.length) {
+                if (quotes[i - 1].close - quotes[i] < 0) {
+                    consecutiveGainers.push(true);
+                }
+                break;
+            }
             if (quotes[i].close - quotes[i + 1].close < 0) {
                 consecutiveGainers.push(true);
             } else {
                 consecutiveGainers.push(false);
             }
         }
-        return !consecutiveGainers.some((gainer) => !gainer);
+        return !consecutiveGainers.some((gainer) => !gainer); // resolves as true if there is a single "false" entry in the array - but once we add ! it resolves as false which is the desired outcome as if there is a false that means at one point the stock wasnt gaining
     }
 
-    const consecutiveGainers = await Promise.all(
-        gainers.filter(async (gainer) => {
-            const historicalData = await getHistoricalData(
+    const historicalData = await Promise.all(
+        gainers.map(async (gainer) => {
+            const data = await getHistoricalData(
                 gainer,
-                "2023-01-25",
-                "2023-01-31"
+                getDateXDaysAgo(13),
+                today
             );
-            return isConsecutiveGainer(historicalData);
-
-            // This filter is absolutely fucked it keeps returning the same data despite changes to isConsecutiveGainer(), will need to investigate how to fix this later on
+            if (isConsecutiveGainer(data)) {
+                return gainer;
+            }
         })
     );
-    console.log(
-        isConsecutiveGainer([
-            {
-                close: 102.419998,
-            },
-            {
-                close: 101.129997,
-            },
-            {
-                close: 103.199997,
-            },
-            {
-                close: 103.209999,
-            },
-        ])
-    );
+    const consecutiveGainers = historicalData.filter((gainer) => gainer);
+
     res.send(consecutiveGainers);
+});
+
+app.get("/consecutiveLosers", async function (req, res) {
+    //check historical data for the last 8 days and store that in an array in mongodb :)
+    const losers = await getCleanedStockData();
+
+    function isConsecutiveLoser(quotes) {
+        const consecutiveLosers = [];
+        for (let i = 0; i < quotes.length; i++) {
+            if (i + 1 === quotes.length) {
+                if (quotes[i - 1].close - quotes[i] > 0) {
+                    consecutiveLosers.push(true);
+                }
+                break;
+            }
+            if (quotes[i].close - quotes[i + 1].close > 0) {
+                consecutiveLosers.push(true);
+            } else {
+                consecutiveLosers.push(false);
+            }
+        }
+        return !consecutiveLosers.some((loser) => !loser);
+    }
+
+    const historicalData = await Promise.all(
+        losers.map(async (loser) => {
+            const data = await getHistoricalData(
+                loser,
+                getDateXDaysAgo(7),
+                today
+            );
+            if (isConsecutiveLoser(data)) {
+                return loser;
+            }
+        })
+    );
+    const consecutiveLosers = historicalData.filter((loser) => loser);
+
+    res.send(consecutiveLosers);
 });
 
 module.exports = app;
