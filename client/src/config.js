aa031b1b38ebc08e6be4e2a083353bbb5d22996d
GiorgioBaz <giorgio.bazouni@gmail.com>
2023-12-11 21:51:06 +1100

    Updated backend url

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000/stocks",
+    baseURL: "https://wolf-of-happ-st.onrender.com/stocks",
 });
